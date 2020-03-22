a9822fc04aecb8eb35ef29b7ae7c34f1ed0ba63e
TheRealRolandDeschain <johannes.zellinger@gmx.at>
2020-03-22 14:07:38 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiscordBannerBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
